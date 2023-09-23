c3dabbd5969a751048932aa745ca7cbd1cddc2c9
HelloWorldDogs223 <yonghyeogi377@gmail.com>
2023-09-24 00:10:10 +0900

    d

--- client/src/pages/ReviewPage.jsx
+++ client/src/pages/ReviewPage.jsx
@@ -1,7 +1,7 @@
 import Box from "@mui/material/Box";
 import Rating from "@mui/material/Rating";
 import { useEffect, useRef, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 export const Title = styled.p`
@@ -54,16 +54,72 @@ export const TextCount = styled.p`
   font-size: 12px;
   color: darkgray;
   position: absolute;
-  top: 66%;
-  right: 35%;
+  top: 61%;
+  right: 44%;
 `;
 
 export const Colored = styled.span`
   color: #2ab7c0;
 `;
 
-export const InputButton = styled.input`
-  color: red;
+const InputButton = styled.input`
+  display: none;
+`;
+
+const Label = styled.label`
+  cursor: pointer;
+`;
+const ImgContainer = styled.div`
+  margin-top: 10px;
+  width: 91px;
+  height: 91px;
+  position: relative;
+  border: 1px solid gray;
+`;
+
+const ImgTaken = styled.img`
+  width: 91px;
+  height: 91px;
+`;
+
+const Camera = styled.img`
+  position: absolute;
+  top: 40%;
+  left: 40%;
+  z-index: 2;
+  width: 20px;
+  height: 20px;
+`;
+
+const ImgAndImgContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  align-items: center;
+`;
+
+const EndSpan = styled.span`
+  color: #2ab7c0;
+  cursor: pointer;
+`;
+
+const BackContainer = styled.div`
+  display: flex;
+  align-items: start;
+  width: 375px;
+`;
+
+const LeftArrow = styled.img`
+  cursor: pointer;
+`;
+
+const Ximg = styled.img`
+  position: absolute;
+  z-index: 15;
+  top: 9%;
+  right: 5%;
+  cursor: pointer;
 `;
 
 export default function ReviewPage() {
@@ -73,9 +129,33 @@ export default function ReviewPage() {
   const [number, setNumber] = useState(0);
   const [isDone, setIsDone] = useState(false);
   const [img, setImg] = useState("");
+  const [isImg, setIsImg] = useState(false);
+
+  const navigate = useNavigate();
 
   const imgRef = useRef(null);
   const imgRef2 = useRef(null);
+  const imgRef3 = useRef(null);
+
+  const xHandler = (event) => {
+    const label = event.target.parentNode;
+    const originalHtmlFor = label.htmlFor;
+
+    // htmlFor 속성 잠시 비우기
+    label.htmlFor = "";
+
+    // 이미지 초기화
+    imgRef2.current.src =
+      "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
+    imgRef.current.src =
+      "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
+
+    // htmlFor 속성 원래대로 복구 (딜레이 추가)
+    setTimeout(() => {
+      label.htmlFor = originalHtmlFor;
+    }, 0);
+    setIsImg(false);
+  };
 
   const saveImgFile = async () => {
     const file = imgRef.current.files[0];
@@ -83,15 +163,18 @@ export default function ReviewPage() {
       const formData = new FormData();
       formData.append("multipartFile", file);
       setImg(URL.createObjectURL(file));
+      imgRef2.current.src = URL.createObjectURL(file);
       // @todo : axios post
       alert("수정완료되었습니다!");
+      imgRef3.current.src =
+        "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
+      setIsImg(true);
     } catch {
       alert("에러가 발생하였습니다. 다시 시도해주세요.");
     }
   };
 
   const textHandler = (e) => {
-    console.log(e.target);
     setNumber(e.target.value.length);
   };
 
@@ -105,10 +188,16 @@ export default function ReviewPage() {
 
   return (
     <>
+      <BackContainer>
+        <LeftArrow
+          onClick={() => navigate("/detail", { state: item })}
+          src="/images/back.svg"
+        />
+      </BackContainer>
       <div>
         <TopDiv>
           <Title>리뷰 작성</Title>
-          <End>{isDone ? <span>완료</span> : <span>완료</span>}</End>
+          <End>{isDone ? <EndSpan>완료</EndSpan> : <span>완료</span>}</End>
         </TopDiv>
         <DESC>
           <Img src={item.img} alt="아이템 이미지" />
@@ -138,13 +227,26 @@ export default function ReviewPage() {
           <TextCount>{number}/300</TextCount>
         </div>
       </div>
-      <InputButton
-        type="file"
-        accept="image/*"
-        onChange={saveImgFile}
-        ref={imgRef}
-      />
-      <img src="/images/camera.svg" ref={imgRef2} />
+      <ImgAndImgContainer>
+        <ImgContainer>
+          <Label htmlFor="fileInput">
+            <ImgTaken
+              src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+              ref={imgRef2}
+              alt=""
+            />
+            {isImg && <Ximg onClick={xHandler} src="/images/xsign.svg" />}
+            {!isImg && <Camera ref={imgRef3} src="/images/Camera.svg" />}
+          </Label>
+          <InputButton
+            id="fileInput"
+            type="file"
+            accept="image/*"
+            onChange={saveImgFile}
+            ref={imgRef}
+          />
+        </ImgContainer>
+      </ImgAndImgContainer>
     </>
   );
 }
